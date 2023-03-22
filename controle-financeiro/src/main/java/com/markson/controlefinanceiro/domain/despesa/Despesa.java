0b3d9712f32c8b3528dbd6a40082b8a2c3624a5c
Markson Arguello <marksonarguello@gmail.com>
2023-03-21 21:36:45 -0300

    Criando entidade Despesa

---
+++ controle-financeiro/src/main/java/com/markson/controlefinanceiro/domain/despesa/Despesa.java
@@ -0,0 +1,25 @@
+package com.markson.controlefinanceiro.domain.despesa;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "Despesa")
+@Table(name = "despesas")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Despesa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+    private BigDecimal valor;
+    private LocalDateTime data;
+}
---
+++ controle-financeiro/src/main/resources/db/migration/V2__create-table-despesas.sql
@@ -0,0 +1,6 @@
+CREATE TABLE despesas(
+    id SERIAL PRIMARY KEY,
+    descricao TEXT,
+    valor NUMERIC,
+    data TIMESTAMP
+);
