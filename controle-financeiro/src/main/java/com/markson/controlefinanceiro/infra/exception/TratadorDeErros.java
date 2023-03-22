2e51cda9d109e7a602d69fc1d4e67b1b89f7e6e3
Markson Arguello <marksonarguello@gmail.com>
2023-03-22 15:38:00 -0300

    Tratando exceptions da API

---
+++ controle-financeiro/src/main/java/com/markson/controlefinanceiro/infra/exception/TratadorDeErros.java
@@ -0,0 +1,43 @@
+package com.markson.controlefinanceiro.infra.exception;
+
+import com.markson.controlefinanceiro.domain.exception.ValidacaoException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class TratadorDeErros {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity tratarErroJsonRequest(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(new DadosErroValidacao("body", "Malformed JSON request"));
+    }
+
+    @ExceptionHandler(ValidacaoException.class)
+    public ResponseEntity tratarErroRegraDeNegocio(ValidacaoException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(ex.getMessage());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+}
