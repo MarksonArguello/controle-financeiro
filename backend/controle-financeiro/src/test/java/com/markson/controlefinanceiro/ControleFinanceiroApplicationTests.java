ecc8104b94130a6c3830d9ccc725ce027a01abbd
Markson Arguello <marksonarguello@gmail.com>
2024-02-29 13:08:34 -0300

    Especificando classe de teste do ControleFinanceiroApplicationTests

--- backend/controle-financeiro/src/test/java/com/markson/controlefinanceiro/ControleFinanceiroApplicationTests.java
+++ backend/controle-financeiro/src/test/java/com/markson/controlefinanceiro/ControleFinanceiroApplicationTests.java
@@ -3,7 +3,7 @@ package com.markson.controlefinanceiro;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ControleFinanceiroApplicationTests.class)
 class ControleFinanceiroApplicationTests {
 
 	@Test
